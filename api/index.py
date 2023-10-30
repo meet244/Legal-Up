fa686208888cd76cb7a1ed024e98c6cf18bd1ddd
Meet Patel <meet2005pokar@gmail.com>
2023-10-30 19:09:19 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 import requests
 import json
 import threading
@@ -37,7 +37,8 @@ from flask_cors import CORS
 
 # FUNCTIONS
 
-def translateChecks(text:str) -> str:
+
+def translateChecks(text: str) -> str:
     translator = Translator()
 
     # Text you want to translate
@@ -48,9 +49,9 @@ def translateChecks(text:str) -> str:
     print(text)
     detected_lang = translator.detect(text).lang
 
-    if(detected_lang!='en'):
-    # Translate the text to another language (e.g., Spanish)
-        translated_text = translator.translate(text, dest='en')
+    if detected_lang != "en":
+        # Translate the text to another language (e.g., Spanish)
+        translated_text = translator.translate(text, dest="en")
         return translated_text
     return text
     # print(f"Original Text: {text_to_translate}")
@@ -58,101 +59,134 @@ def translateChecks(text:str) -> str:
     # print(f"Translated Text: {translated_text.text}")
 
 
-def getMatchScore (lawyerObj, clientReqObj) : 
+def getMatchScore(lawyerObj, clientReqObj):
     totalPoints = 0
-    
+
     # Points for Case Match
     caseTypePoints = 0
-    for cCaseType in clientReqObj["caseType"]: 
-        for lCaseType in lawyerObj["speciality"] : 
-            if (lCaseType == cCaseType) : 
-                if (caseTypePoints == 0) : caseTypePoints += 10
-                else : caseTypePoints += 5
-                
+    for cCaseType in clientReqObj["caseType"]:
+        for lCaseType in lawyerObj["speciality"]:
+            if lCaseType == cCaseType:
+                if caseTypePoints == 0:
+                    caseTypePoints += 10
+                else:
+                    caseTypePoints += 5
+
     totalPoints += caseTypePoints
-            
+
     # Points for Languages
     languagePoints = 0
-    for cLang in clientReqObj["languages"]: 
-        for lLang in lawyerObj["languages"] : 
-            if (lLang == cLang) : 
-                if (languagePoints == 0) : languagePoints += 10
-                else : languagePoints += 2
-                
+    for cLang in clientReqObj["languages"]:
+        for lLang in lawyerObj["languages"]:
+            if lLang == cLang:
+                if languagePoints == 0:
+                    languagePoints += 10
+                else:
+                    languagePoints += 2
+
     totalPoints += languagePoints
-    
+
     # Calculating Budget Points
     budgetPoints = 0
     a = max(clientReqObj["budget"], lawyerObj["price"])
     b = min(clientReqObj["budget"], lawyerObj["price"])
-    diff = a-b
-    if (diff < 250) : 
-        diff = int(diff/250)
+    diff = a - b
+    if diff < 250:
+        diff = int(diff / 250)
         budgetPoints += diff
-        
+
     totalPoints += budgetPoints
-    
-    
+
     # Caluclating Location Points
     locationPoint = 0
-    if (clientReqObj["location"] == lawyerObj["location"]) : locationPoint += 10
+    if clientReqObj["location"] == lawyerObj["location"]:
+        locationPoint += 10
     totalPoints += locationPoint
-    
+
     # Adding Lawyer Rating Points
-    totalPoints += lawyerObj["rating"]*2
-    
-    
-    
+    totalPoints += lawyerObj["rating"] * 2
+
     # Returning TotalPoints
     return totalPoints
 
-def sortFunction (t) : 
+
+def sortFunction(t):
     return t[0]
 
-def recommendedLawyers(clientReqObj) -> list : 
 
-    with open('lawyers.json','r') as f : 
-        lawyers = json.load(f)
+def recommendedLawyers(clientReqObj) -> list:
+    # with open('lawyers.json','r') as f :
+    #     lawyers = json.load(f)
+    lawyers = [
+        {
+            "name": "Anay Ahluwalia",
+            "id": "bc0ea250-6e34-4588-ba7a-a7ded6179293",
+            "experience": 20,
+            "speciality": ["Corporate", "Consumer Protection", "Labor"],
+            "location": "Hyderabad",
+            "clientType": "Large Corporations",
+            "rating": 5.0,
+            "jurisdiction": "Supreme Court",
+            "price": 420.69,
+            "avgDaysOfCompletion": 117,
+            "languages": ["Hindi", "Telugu"],
+            "gender": "M",
+        },
+        {
+            "name": "Arnav Dubey",
+            "id": "13c65fee-1467-481d-b0d6-6b3b8da2dfbf",
+            "experience": 29,
+            "speciality": ["Criminal", "Tax", "Intellectual Property"],
+            "location": "Delhi",
+            "clientType": "Small Businesses",
+            "rating": 2.0,
+            "jurisdiction": "Specialized Court",
+            "price": 435.66,
+            "avgDaysOfCompletion": 119,
+            "languages": ["Urdu", "Kannada", "Tamil"],
+            "gender": "M",
+        },
+    ]
 
     # recommendedMaleLawyers = []
     # recommendedFemaleLawyers = []
     lawyerList = []
-    
-    for lawyer in lawyers :
-        score = getMatchScore(lawyer,clientReqObj)
-        lawyerList.append((score,lawyer))
-        # if (lawyer["gender"] == "F") : 
+
+    for lawyer in lawyers:
+        score = getMatchScore(lawyer, clientReqObj)
+        lawyerList.append((score, lawyer))
+        # if (lawyer["gender"] == "F") :
         #     recommendedFemaleLawyers.append( tuple( (score, lawyer) ) )
-        # elif (lawyer["gender"] == "M") : 
-        #     recommendedMaleLawyers.append( tuple( (score, lawyer) ) ) 
-            
+        # elif (lawyer["gender"] == "M") :
+        #     recommendedMaleLawyers.append( tuple( (score, lawyer) ) )
+
     # recommendedFemaleLawyers = sorted(recommendedFemaleLawyers, key= sortFunction, reverse=True)
     # recommendedMaleLawyers = sorted(recommendedMaleLawyers, key= sortFunction, reverse=True)
-    
-    # for i in range(20) : 
-    #     lawyerList.append(recommendedFemaleLawyers[i])
-    # for i in range(20) : 
-    #     lawyerList.append(recommendedMaleLawyers[i])
-        
-    lawyerList = sorted(lawyerList, key= sortFunction, reverse=True)
-    finalList = [obj for (s,obj) in lawyerList]    
 
-        
+    # for i in range(20) :
+    #     lawyerList.append(recommendedFemaleLawyers[i])
+    # for i in range(20) :
+    #     lawyerList.append(recommendedMaleLawyers[i])
+
+    lawyerList = sorted(lawyerList, key=sortFunction, reverse=True)
+    finalList = [obj for (s, obj) in lawyerList]
+
     return finalList[0:15]
 
 
-def findLocation(latitude:float,longitude:float) -> str:
+def findLocation(latitude: float, longitude: float) -> str:
     geolocator = Nominatim(user_agent="my-app")
     location = geolocator.reverse(f"{latitude}, {longitude}")
 
-    return(location.raw['address']['city_district'])  # City
-    print(location.raw['address']['state']) # State
+    return location.raw["address"]["city_district"]  # City
+    print(location.raw["address"]["state"])  # State
 
 
 def preprocess_text(text):
     text = text.lower()
-    text = text.replace('[^\w\s]', '')
+    text = text.replace("[^\w\s]", "")
     return text
+
 
 # def getCaseType(query:str) -> [str]:
 
@@ -188,9 +222,9 @@ def preprocess_text(text):
 #         return r
 
 # def getClientType(query:str) -> [str]:
-    
+
 #     X_new = vectorizer.transform([query])
-    
+
 #     # Make predictions using the loaded model
 #     predictions = clientClassify.predict(X_new)
 #     # You can also use model.predict_proba(X_new) if you need probability scores
@@ -200,10 +234,11 @@ def preprocess_text(text):
 app = Flask(__name__)
 CORS(app)
 
-app.static_url_path='/static'
-app.static_folder='static'
+app.static_url_path = "/static"
+app.static_folder = "static"
 
-@app.route('/')
+
+@app.route("/")
 def hi():
     # ip = request.remote_addr  # Get the IP address of the user
     # response = requests.get(f"http://api.ipstack.com/{ip}?access_key=bbccbd3f1f37d9562e10a53cd69445f7")
@@ -212,56 +247,58 @@ def hi():
     # city = data.get('city', 'Unknown')
     # state = data.get('region_name', 'Unknown')
     # country = data.get('country_name', 'Unknown')
-    
-    return jsonify(f'Hello, Mumbai!')
 
-@app.route('/1')
+    return jsonify(f"Hello, Mumbai!")
+
+
+@app.route("/1")
 def hi2():
     # file = os.path.join(os.getcwd(), 'model', 'case_vectorizer.pkl')
     # with open(file, 'r', encoding='utf-8') as f:
     #     data = json.load(f)
-        
-    return jsonify(f'Hello, Mumbai!')
+
+    return jsonify(f"Hello, Mumbai!")
 
 
-@app.route('/api/rate')
+@app.route("/api/rate")
 def rate():
     r = request.json.get("rate")
     r = float(r)
-    
-    if(r>5):return 'Error',400
-    
-    return jsonify('ok')
+
+    if r > 5:
+        return "Error", 400
+
+    return jsonify("ok")
 
 
 # @app.route('/api/query', methods=['POST'])
-@app.route('/api/query',methods=['POST','GET'])
+@app.route("/api/query", methods=["POST", "GET"])
 def query():
     # Translate
-    q = request.json.get('query')
+    q = request.json.get("query")
 
     q = translateChecks(q)
 
     # Case Classify
     # cases = getCaseType(q)
-    cases = ['']
+    cases = [""]
 
     # Client Classify
     # clientType = getClientType(q)
-    clientType =[""]
+    clientType = [""]
 
     # Find Location
-    lat = request.json.get('latitude')
-    lon = request.json.get('longitude')
-    state = findLocation(float(lat),float(lon))
-    
+    lat = request.json.get("latitude")
+    lon = request.json.get("longitude")
+    state = findLocation(float(lat), float(lon))
+
     # Recommend Model
     client = {
-        "clientType" : clientType,
-        "caseType" : cases,
-        "location" : state,
-        "languages" : ["English","Hindi"],
-        "budget" : 300
+        "clientType": clientType,
+        "caseType": cases,
+        "location": state,
+        "languages": ["English", "Hindi"],
+        "budget": 300,
     }
     finals = recommendedLawyers(client)
 
@@ -269,59 +306,52 @@ def query():
     # process query
     # example result
     ans = {
-    "name": "Jayesh Kapur",
-    "experience": 23,
-    "typesOfCases": [
-        "Immigration Law",
-        "Medical Law"
-    ],
-    "location": "Bangalore",
-    "possibleClients": "Large Corporations",
-    "ClientFeedback": 5.0,
-    "jurisdiction": "High Court",
-    "price": 242.45,
-    "avgDaysOfDisposal": 100,
-    "languages": [
-        "Telugu",
-        "Urdu",
-        "Tamil"
-    ]
-}
+        "name": "Jayesh Kapur",
+        "experience": 23,
+        "typesOfCases": ["Immigration Law", "Medical Law"],
+        "location": "Bangalore",
+        "possibleClients": "Large Corporations",
+        "ClientFeedback": 5.0,
+        "jurisdiction": "High Court",
+        "price": 242.45,
+        "avgDaysOfDisposal": 100,
+        "languages": ["Telugu", "Urdu", "Tamil"],
+    }
     return jsonify([ans])
 
 
-@app.route('/api/form',methods=['POST','GET'])
+@app.route("/api/form", methods=["POST", "GET"])
 def form():
     try:
         l = {
-            "name": str(request.json.get('name')),
+            "name": str(request.json.get("name")),
             "id": str(uuid.uuid4()),
-            "experience": int(request.json.get('experience')),
-            "speciality": (request.json.get('speciality')),
-            "location": str(request.json.get('location')),
-            "clientType": str(request.json.get('clientType')),
+            "experience": int(request.json.get("experience")),
+            "speciality": (request.json.get("speciality")),
+            "location": str(request.json.get("location")),
+            "clientType": str(request.json.get("clientType")),
             "rating": random.uniform(0.0, 5.0),
-            "jurisdiction": str(request.json.get('jurisdiction')),
-            "price": float(request.json.get('price')),
-            "avgDaysOfCompletion": int(request.json.get('avgDaysOfCompletion')),
-            "languages": (request.json.get('languages')),
-            "gender": str(request.json.get('gender'))
+            "jurisdiction": str(request.json.get("jurisdiction")),
+            "price": float(request.json.get("price")),
+            "avgDaysOfCompletion": int(request.json.get("avgDaysOfCompletion")),
+            "languages": (request.json.get("languages")),
+            "gender": str(request.json.get("gender")),
         }
 
-        with open('lawyers.json', 'r') as file:
+        with open("lawyers.json", "r") as file:
             data = json.load(file)
 
         data.append(l)
         print(data[-1])
-        with open('lawyers.json', 'w') as file:
+        with open("lawyers.json", "w") as file:
             json.dump(data, file, indent=4)
 
         return "ok"
     except:
-        return "",400
-
+        return "", 400
 
     return "ok"
 
-if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
